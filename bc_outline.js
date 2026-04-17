c8c6f09dfe1799d55709117635a0da365f34e2bf
Lyle Goldman <gizmobro@hotmail.com>
2026-04-16 22:16:18 -0400

    Finished 12.2.

--- bc_outline.js
+++ bc_outline.js
@@ -53,6 +53,12 @@ function createList(source, outlineList) {
     // Headings for the outline.
     var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
 
+    // Previous Level of the Headings.
+    var prevLevel = 0;
+
+    // Running total of the article headings.
+    var headNum = 0;
+
     /* Loop through all of the child nodes of source article
        until no child nodes are left. */
     for(var n = source.firstChild; n !== null; n = n.nextSibling) {
@@ -60,9 +66,52 @@ function createList(source, outlineList) {
         var headLevel = headings.indexOf(n.nodeName);
 
         if(headLevel !== -1) {
+            // Add an id to the heading if it is missing.
+            headNum++;
+            if(n.hasAttribute("id") === false) {
+                n.setAttribute("id", "head" + headNum);
+            }
+            
             var listElem = document.createElement("li");
-            listElem.innerHTML = n.firstChild.nodeValue;
-            outlineList.appendChild(listElem);
+
+            // Create hypertext links to the document headings.
+            var linkElem = document.createElement("a");
+            linkElem.innerHTML = n.innerHTML;
+            linkElem.setAttribute("href", "#" + n.id);
+
+            // Append the hypertext link to the list item.
+            listElem.appendChild(linkElem);
+
+            if(headLevel === prevLevel) {
+                // Append the list item to the current list.
+                outlineList.appendChild(listElem);
+            }
+            else if(headLevel > prevLevel) {
+                // Start a new nested list.
+                var nestedList = document.createElement("ol");
+                nestedList.appendChild(listElem);
+
+                // Append nested list to last item in the current list.
+                outlineList.lastChild.appendChild(nestedList);
+
+                // Change the current list ti the nested list.
+                outlineList = nestedList;
+            }
+            else {
+                // Append the list item to a higher list.
+                // Calculate the difference between the current and previous level.
+                var levelUp = prevLevel - headLevel;
+
+                // Go up to the higher level.
+                for(var i = 1; i <= levelUp; i++) {
+                    outlineList = outlineList.parentNode.parentNode;
+                }
+
+                outlineList.appendChild(listElem);
+            }
+
+            // Update the value of prevLevel.
+            prevLevel = headLevel;
         }
     }
 }
